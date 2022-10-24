2ff4e3b04f9b9d0f2f517fdfec8b7707f24a0eb7
Zoe Gonzalez <zusolaris.personal@gmail.com>
2022-10-24 15:44:11 -0700

    added api functionality and search bar

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 <!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
 
 ## Architecture
-<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+<img src='./img/wrrc.png'>
 
 ## Change Log
 <!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
@@ -19,12 +19,23 @@
 
 ## Credit and Collaborations
 
-Name of feature:Add Location API
+Name of feature:Set Up Keys and Security
 
-Estimate of time needed to complete: 3 Hours
+Estimate of time needed to complete: 1 Hours
 
 Start time: 1:36
 
-Finish time: 
+Finish time: 2:15
 
-Actual time needed to complete: 
+Actual time needed to complete: 51 Mins
+
+Name of feature:City Search Bar
+
+Estimate of time needed to complete: 2 Hours
+
+Start time: 2:16
+
+Finish time: 3:39
+
+Actual time needed to complete: 1 Hour 24 Mins
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityData: [],
+      city: '',
+      error: false,
+      errorMsg: ''
+    }
+  }
+
+  getCity = async (e) => {
+    e.preventDefault();
+    try {
+      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_CITYLOC_API_KEY}&q=${this.state.city}&format=json`
+
+      let cityData = await axios.get(url);
+
+      this.setState({
+        cityData: cityData.data[0],
+        error: false
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMsg: error.Message
+      })
+      console.log('test');
+    }
+
+
+
+
+  }
+
+  render() {
+    return (
+      <Form onSubmit={this.getCity}>
+        <Form.Group className='search'>
+          <Form.Label>City Look Up</Form.Label>
+          <Form.Control type='text' placeholder='Type a city here!' />
+          <Form.Text className='undertext'>
+            Search from anywhere in the world!
+          </Form.Text>
+        </Form.Group>
+        <Button variant='info' type='submit'>
+          Submit
+        </Button>
+      </Form>
+    )
+  }
 }
 
 export default App;

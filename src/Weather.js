2a7afce0b10cba4892ca7f952df0f4678fdbef0f
Zoe Gonzalez <zusolaris.personal@gmail.com>
2022-10-25 23:26:31 -0700

    linked server to front end

--- src/Weather.js
+++ src/Weather.js
@@ -1,35 +1,23 @@
 import React from 'react';
-import axios from 'axios';
+
 
 class Weather extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      weatherData: [],
-      weatherError: false,
-      wErrorMsg: '',
-    }
-  }
-
-getWeather = async (e) => {
-  e.preventDefault();
-
-  try {
-    let url = `${process.env.REACT_APP_SERVER}/weather?cityName=${this.state.cityData.display_name}&?lat=${this.state.city.lat}&lon=${this.state.city.lon}`
-
-    let weatherData = await axios.get(url);
-
-    this.setState({
-      weatherError: false,
-    });
-  } catch (error) {
-    this.setState({
-      weatherError: true,
-      wErrorMsg: error.message
-    })
-    console.log('test');
-  }
-
-}
+  render() {
+    return (
+      <>
+      {this.props.weatherData.map((data) => (
+          <>
+            <p>Weather Data</p>
+            <p>Weather Date</p>
+            <div>{data.date}</div>
+            <p>Weather Details</p>
+            <div>{data.desc}</div>
+          </>
+    ))}
+   </>
+  )}
+  
 }
 export default Weather;
+
+
